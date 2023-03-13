478a4b7b1581c271c4016f710e0f62e3f3dcc751
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-13 14:07:26 +0100

    Hide senstitive info

--- home/settings.py
+++ home/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c_a66mjj^ozh0cq198#)j6#aya*1r&+h-$b4i4jw&%kdb$h(qj'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
