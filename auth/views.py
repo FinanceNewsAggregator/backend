9305c0cb43bb4dcb340f6c731fc30e8d1bc0a961
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-13 14:53:22 +0100

    Preparing Registeration Module - Imported Serializer classes

--- auth/views.py
+++ auth/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
+from home.auth.serializer import *
 
 
 class UserRegistrationView(CreateAPIView):
