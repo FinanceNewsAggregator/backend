27f50fdbf117e96f71c53adc287b8782342729b3
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-14 15:48:24 +0100

    Added basic Imports in News Module

--- news/views.py
+++ news/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+import requests
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.conf import settings
+from django.contrib.auth import get_user_model
