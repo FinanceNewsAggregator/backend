9df2a35083cd874c6e708e254f021d4d3b4c164b
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-14 15:50:28 +0100

    Used get_user_model()

--- news/views.py
+++ news/views.py
@@ -3,3 +3,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.conf import settings
 from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
