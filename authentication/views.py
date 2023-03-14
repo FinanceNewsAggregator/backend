4770a1ddc5bf8d02c9373e4c0807adfdcb11e2aa
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-14 14:09:26 +0100

    Imports for Login and Logout

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 from .serializer import *
 
@@ -10,3 +16,5 @@ class UserRegistrationView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
