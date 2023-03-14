41133622402f583647a97a3261f936f070ece962
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-14 10:59:21 +0100

    Bug Fixed Sucessfully

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Preference(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='preference')
+    tags = models.JSONField()
--- authentication/serializer.py
+++ authentication/serializer.py
@@ -1,21 +1,31 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from .models import Preference
+
+
+class PreferenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Preference
+        fields = ('tags',)
+
+
 # Serializer that defines the fields for the user registration form
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    tags = serializers.ListField(child=serializers.CharField())
+    preference = PreferenceSerializer()
 
-# Tags field for the user to select their preferred news topics
     class Meta:
         model = User
-        fields = ('email', 'password', 'tags')
+        fields = ('email', 'password', 'preference')
 
     def create(self, validated_data):
+        preference_data = validated_data.pop('preference')
         user = User.objects.create(
             username=validated_data['email'],
             email=validated_data['email']
         )
         user.set_password(validated_data['password'])
+        preference = Preference.objects.create(user=user, **preference_data)
         user.save()
         return user
