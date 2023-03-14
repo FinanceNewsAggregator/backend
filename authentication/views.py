5fc536ab61b3347f7c214656eb1484e5806f5619
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-14 14:14:08 +0100

    Created class for LoginView

--- authentication/views.py
+++ authentication/views.py
@@ -18,3 +18,14 @@ class UserRegistrationView(CreateAPIView):
     permission_classes = [AllowAny]
 
 
+class LoginView(APIView):
+    def post(self, request, format=None):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        if email is None or password is None:
+            return Response({'error': 'Please provide both email and password'}, status=status.HTTP_400_BAD_REQUEST)
+        user = authenticate(username=email, password=password)
+        if not user:
+            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=status.HTTP_200_OK)
