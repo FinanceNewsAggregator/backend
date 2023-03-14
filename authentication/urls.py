759aba87f501648102be09b3f8a6bd453c277b5f
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-14 14:18:32 +0100

    Added Login and Logout to urlPatterns I

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,4 +5,5 @@ from .views import UserRegistrationView, LoginView, LogoutView
 urlpatterns = [
     path('register/', UserRegistrationView.as_view()),
     path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view())
 ]
