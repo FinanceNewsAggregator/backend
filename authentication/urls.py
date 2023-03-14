9482daee723e4f2081bfc2050fae4cb1ce871ad5
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-14 14:17:01 +0100

    Imported views for Login and Logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegistrationView
+from .views import UserRegistrationView, LoginView, LogoutView
 
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view()),
+    path('login/', .as_view()),
 ]
